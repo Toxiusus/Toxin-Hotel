ccd1258d71d320412ca771825baac6f40658efb6
Toxiusus <icek1lla2323@gmail.com>
2024-12-26 15:25:58 +0300

    add footer and styles footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,128 @@
+<footer class="footer">
+  <div class="footer__container">
+    <div class="footer__top container">
+      <div class="footer__about">
+        <a href="#">
+          <img src="/img/Logo.6d39cdbf.svg" alt="logo" />
+        </a>
+        <p class="footer__about-text">
+          Бронирование номеров в лучшем отеле 2019 года по версии ассоциации
+          «Отельные взгляды»
+        </p>
+      </div>
+      <div class="footer__column">
+        <h3 class="footer__title">навигация</h3>
+        <ul class="list-reset footer__list">
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              О нас
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Новости
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Служба поддержки
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Услуги
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer__column">
+        <h3 class="footer__title">о нас</h3>
+        <ul class="list-reset footer__list">
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              О сервисе
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Наша команда
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Вакансии
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Инвесторы
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer__column">
+        <h3 class="footer__title">Служба поддержки</h3>
+        <ul class="list-reset footer__list">
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Соглашения
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Сообщества
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="#">
+              Связь с нами
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer__subscribe">
+        <h3 class="footer__title">навигация</h3>
+        <p class="footer__subscribe-text">
+          Получайте специальные предложения и новости сервиса
+        </p>
+        <form action="footer__form form">
+          <input
+            class="footer__input form__input"
+            type="email"
+            name="email"
+            required=""
+            placeholder="Email"
+          />
+          <button class="btn-reset footer__btn">
+            <img src="/img/arrow-right.9baa87c8.svg" alt="arrow" />
+          </button>
+        </form>
+        <p></p>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <div class="footer__bottom-container">
+        <span class="footer__copyright">
+          Copyright © 2018 Toxin отель. Все права защищены.
+        </span>
+        <ul class="list-reset footer__social">
+          <li class="footer__social-item">
+            <a class="footer__social-link" href="#">
+              <img src="/img/twitter.8b6cae30.svg" alt="twitter" />
+            </a>
+          </li>
+          <li class="footer__social-item">
+            <a class="footer__social-link" href="#">
+              <img src="/img/facebook.6ecf37c1.svg" alt="facebook" />
+            </a>
+          </li>
+          <li class="footer__social-item">
+            <a class="footer__social-link" href="#">
+              <img src="/img/instagram.cb272556.svg" alt="instagram" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>;
