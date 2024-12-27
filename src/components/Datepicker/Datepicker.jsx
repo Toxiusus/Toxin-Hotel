8bad9d0b767a04e792f027108802faa97c5f1701
Toxiusus <icek1lla2323@gmail.com>
2024-12-27 14:22:56 +0300

    fix

--- src/components/Datepicker/Datepicker.jsx
+++ src/components/Datepicker/Datepicker.jsx
@@ -1,18 +1,3 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
-
-function Datepicker() {
-  const Example = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-      />
-    );
-  };
-}
+function DatePicker() {}
 
 export default DatePicker;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,10 @@
 import Dropdown from "../../components/Dropdown/Dropdown";
-import DatePicker from "../../components/Datepicker/Datepicker";
 import "./Landing.scss";
 
 function Landing() {
   return (
     <section className="landing">
       <div className="landing__container">
-        <Datepicker/>
         <Dropdown />
         <p className="landing__text">
           Лучшие номера для вашей работы, отдыха и просто вдохновения
