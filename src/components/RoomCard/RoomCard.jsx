6e0452dc46077413e8f946473d11c774261ae07b
Toxiusus <icek1lla2323@gmail.com>
2025-01-14 15:46:16 +0300

    fix

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -3,14 +3,36 @@ import React, { useState } from "react";
 import roomFirst from "../../assets/img/room-0.jpeg";
 import roomSecond from "../../assets/img/room-1.jpeg";
 import roomThird from "../../assets/img/room-2.jpeg";
+import roomFourth from "../../assets/img/room-3.jpeg";
+import roomFifth from "../../assets/img/room-4.jpeg";
+import roomSixth from "../../assets/img/room-5.jpeg";
+import roomSeventh from "../../assets/img/room-6.jpeg";
+import roomEighth from "../../assets/img/room-7.jpeg";
+import roomNinth from "../../assets/img/room-8.jpeg";
+import roomTenth from "../../assets/img/room-9.jpeg";
+import roomEleventh from "../../assets/img/room-10.jpeg";
+import roomTwelfth from "../../assets/img/room-11.jpeg";
 import StarRating from "../StarRating/StarRating";
 
 import "./RoomCard.scss";
 
-function RoomCard() {
-  const images = [roomFirst, roomSecond, roomThird];
+function RoomCard({img,number,lux,price,comments}) {
+  const images = [
+    roomFirst,
+    roomSecond,
+    roomThird,
+    roomFourth,
+    roomFifth,
+    roomSixth,
+    roomSeventh,
+    roomEighth,
+    roomNinth,
+    roomTenth,
+    roomEleventh,
+    roomTwelfth,
+  ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(img);
 
   const prevImage = () => {
     setCurrentIndex(
@@ -25,9 +47,10 @@ function RoomCard() {
   return (
     <div className="card">
       <div className="card__slider">
-        <button className="prev-btn card__slider-btn" onClick={prevImage}>
-          prev
-        </button>
+        <button
+          className="prev-btn card__slider-btn btn-reset"
+          onClick={prevImage}
+        ></button>
         <a href="#">
           <img
             src={images[currentIndex]}
@@ -35,26 +58,27 @@ function RoomCard() {
             className="card__slider-img"
           />
         </a>
-        <button className="next-btn card__slider-btn" onClick={nextImage}>
-          next
-        </button>
+        <button
+          className="next-btn card__slider-btn btn-reset"
+          onClick={nextImage}
+        ></button>
       </div>
       <div className="card__info">
         <div className="card__data">
           <div className="card__number">
             <span className="card__number-symbol">№</span>
-            <span className="card__number-value">888</span>
-            <span className="card__number-text">люкс</span>
+            <span className="card__number-value">{number}</span>
+            <span className="card__number-text">{lux}</span>
           </div>
           <div className="card__price">
-            <span className="card__price-value">9990₽</span>
+            <span className="card__price-value">{price}₽</span>
             <span className="card__price-text">в сутки</span>
           </div>
         </div>
         <div className="card__rating">
           <StarRating />
           <div className="comments">
-            <span className="comments__value">145</span>
+            <span className="comments__value">{comments}</span>
             <span className="comments__text">Отзывов</span>
           </div>
         </div>
--- src/pages/SearchRoom/SearchRoom.jsx
+++ src/pages/SearchRoom/SearchRoom.jsx
@@ -4,11 +4,31 @@ import "./SearchRoom.scss";
 function SearchRoom() {
   return (
     <section className="room">
-      <div className="room__container container">
-        <aside></aside>
+      <div className="room__container">
+        <aside>
+          <a href="#">pisapopa</a>
+        </aside>
         <div className="room__selection">
+          <h1 className="room__selection-title">Номера, которые мы для вас подобрали</h1>
           <div className="cards">
-            <RoomCard/>
+            <RoomCard
+              img={0}
+              number="888"
+              lux="люкс"
+              price="9 990"
+              comments="145"
+            />
+            <RoomCard img={1} number="840" price="9 990" comments="65" />
+            <RoomCard img={2} number="980" price="8 500" comments="35" />
+            <RoomCard img={3} number="856" price="7 300" comments="19" />
+            <RoomCard img={4} number="740" price="6 000" comments="44" />
+            <RoomCard img={5} number="982" price="5 800" comments="56" />
+            <RoomCard img={6} number="678" price="5 500" comments="45" />
+            <RoomCard img={7} number="450" price="5 300" comments="39" />
+            <RoomCard img={8} number="350" price="5 000" comments="77" />
+            <RoomCard img={9} number="666" price="5 000" comments="25" />
+            <RoomCard img={10} number="444" price="5 000" comments="15" />
+            <RoomCard img={11} number="352" price="5 000" comments="55" />
           </div>
         </div>
       </div>
